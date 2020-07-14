9bd95c9eec79fbf78891a59d44e5e3dd15b24526
DougBoyle <dab80@cam.ac.uk>
2020-07-14 13:44:43 +0100

    Split up test file

---
+++ GlidedRoseCSharp/BackstagePassTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp {
+    [TestFixture]
+    public class BackstagePassTest {
+        [Test]
+        public void PassQualityIncreasesByOne_Initially() {
+            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 20};
+            IList<Item> Items = new List<Item> {
+                item
+            };
+            var app = new GildedRose(Items);
+            for (var i = 1; i <= 10; i++) {
+                app.UpdateQuality();
+            }
+            Assert.AreEqual(30, item.Quality);
+        }
+
+        [Test]
+        public void PassQualityIncreasesByTwo_Last10Days() {
+            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20};
+            var Items = new List<Item> {
+                item
+            };
+            var app = new GildedRose(Items);
+            for (var i = 1; i <= 5; i++) {
+                app.UpdateQuality();
+            }
+            Assert.AreEqual(30, item.Quality);
+        }
+
+        [Test]
+        public void PassQualityIncreasesByThree_Last5ays() {
+            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20};
+            var Items = new List<Item> {
+                item
+            };
+            var app = new GildedRose(Items);
+            for (var i = 1; i <= 5; i++) {
+                app.UpdateQuality();
+            }
+            Assert.AreEqual(35, item.Quality);
+        }
+
+        [Test]
+        public void PassQualityZeroAfterSellby() {
+            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20};
+            var Items = new List<Item> {
+                item
+            };
+            var app = new GildedRose(Items);
+            for (var i = 1; i <= 5; i++) {
+                app.UpdateQuality();
+            }
+            Assert.AreEqual(0, item.Quality);
+        }
+    }
+}
--- GlidedRoseCSharp/GildedRoseTest.cs
+++ GlidedRoseCSharp/GildedRoseTest.cs
@@ -5,19 +5,25 @@ namespace csharp {
     [TestFixture]
     public class GildedRoseTest {
         [Test]
-        public void DegradesTwiceAsFast_Once_OutOfDate() {
+        public void DegradesEachDay() {
             var item = new Item {Name = "foo", SellIn = 10, Quality = 40};
             IList<Item> Items = new List<Item> {item};
             var app = new GildedRose(Items);
             for (var i = 1; i <= 10; i++) {
                 app.UpdateQuality();
-                Assert.AreEqual(40 - i, item.Quality);
             }
-
+            Assert.AreEqual(30, item.Quality);
+        }
+        
+        [Test]
+        public void DegradesTwiceAsFast_Once_OutOfDate() {
+            var item = new Item {Name = "foo", SellIn = 0, Quality = 30};
+            IList<Item> Items = new List<Item> {item};
+            var app = new GildedRose(Items);
             for (var i = 1; i <= 10; i++) {
                 app.UpdateQuality();
-                Assert.AreEqual(30 - 2 * i, item.Quality);
             }
+            Assert.AreEqual(10, item.Quality);
         }
 
         [Test]
@@ -27,13 +33,13 @@ namespace csharp {
             var app = new GildedRose(Items);
             for (var i = 1; i <= 10; i++) {
                 app.UpdateQuality();
-                Assert.AreEqual(40 - 2 * i, item.Quality);
             }
+            Assert.AreEqual(20, item.Quality);
 
             for (var i = 1; i <= 5; i++) {
                 app.UpdateQuality();
-                Assert.AreEqual(20 - 4 * i, item.Quality);
             }
+            Assert.AreEqual(0, item.Quality);
         }
 
         [Test]
@@ -65,110 +71,39 @@ namespace csharp {
             for (var i = 1; i <= 50; i++) {
                 app.UpdateQuality();
             }
-
-            foreach (var item in Items) {
-                Assert.AreEqual(-40, item.SellIn);
-            }
+            Assert.AreEqual(-40, item1.SellIn);
+            Assert.AreEqual(-40, item2.SellIn);
+            Assert.AreEqual(-40, item3.SellIn);
+            Assert.AreEqual(-40, item4.SellIn);
         }
 
         [Test]
         public void AgedBrieGetsBetter() {
-            var item = new Item {Name = "Aged Brie", SellIn = 10, Quality = 5};
+            var item = new Item {Name = "Aged Brie", SellIn = 10, Quality = 10};
             IList<Item> Items = new List<Item> {item};
             var app = new GildedRose(Items);
             for (var i = 1; i <= 10; i++) {
                 app.UpdateQuality();
-                Assert.AreEqual(5 + i, item.Quality);
             }
+            Assert.AreEqual(20, item.Quality);
 
             for (var i = 1; i <= 10; i++) {
                 app.UpdateQuality();
-                Assert.AreEqual(15 + 2 * i, item.Quality);
             }
+            Assert.AreEqual(40, item.Quality);
         }
 
         [Test]
         public void MaxQualityIs50() {
-            var item = new Item {Name = "Aged Brie", SellIn = 10, Quality = 5};
-            IList<Item> Items = new List<Item> {item};
+            var item1 = new Item {Name = "Aged Brie", SellIn = 10, Quality = 40};
+            var item2 = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 30, Quality = 40};
+            IList<Item> Items = new List<Item> {item1, item2};
             var app = new GildedRose(Items);
-            for (var i = 1; i <= 100; i++) {
+            for (var i = 1; i <= 30; i++) {
                 app.UpdateQuality();
             }
-
-            Assert.AreEqual(50, item.Quality);
-        }
-
-        [Test]
-        public void SulfurasHasQuality80() {
-            var item1 = new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80};
-            var item2 = new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80};
-            var item3 = new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 1, Quality = 80};
-            IList<Item> Items = new List<Item> {
-                item1, item2, item3
-            };
-            var app = new GildedRose(Items);
-            for (var i = 1; i <= 100; i++) {
-                app.UpdateQuality();
-                Assert.AreEqual(0, item1.SellIn);
-                Assert.AreEqual(-1, item2.SellIn);
-                Assert.AreEqual(1, item3.SellIn);
-                Assert.AreEqual(80, item1.Quality);
-                Assert.AreEqual(80, item2.Quality);
-                Assert.AreEqual(80, item3.Quality);
-            }
-        }
-
-        [Test]
-        public void PassQualityIncreasesByOne_Initially() {
-            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 20};
-            IList<Item> Items = new List<Item> {
-                item
-            };
-            var app = new GildedRose(Items);
-            for (var i = 1; i <= 10; i++) {
-                app.UpdateQuality();
-                Assert.AreEqual(20 + i, item.Quality);
-            }
-        }
-
-        [Test]
-        public void PassQualityIncreasesByTwo_Last10Days() {
-            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20};
-            var Items = new List<Item> {
-                item
-            };
-            var app = new GildedRose(Items);
-            for (var i = 1; i <= 5; i++) {
-                app.UpdateQuality();
-                Assert.AreEqual(20 + 2 * i, item.Quality);
-            }
-        }
-
-        [Test]
-        public void PassQualityIncreasesByThree_Last5ays() {
-            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20};
-            var Items = new List<Item> {
-                item
-            };
-            var app = new GildedRose(Items);
-            for (var i = 1; i <= 5; i++) {
-                app.UpdateQuality();
-                Assert.AreEqual(20 + 3 * i, item.Quality);
-            }
-        }
-
-        [Test]
-        public void PassQualityZeroAfterSellby() {
-            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20};
-            var Items = new List<Item> {
-                item
-            };
-            var app = new GildedRose(Items);
-            for (var i = 1; i <= 10; i++) {
-                app.UpdateQuality();
-                Assert.AreEqual(0, item.Quality);
-            }
+            Assert.AreEqual(50, item1.Quality);
+            Assert.AreEqual(50, item2.Quality);
         }
     }
 }
